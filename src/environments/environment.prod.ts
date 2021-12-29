546e9dfb95d60a1e1d8bc45ace5e4e6db4bf6f05
sangnv113 <37628698+sangnv113@users.noreply.github.com>
2021-12-29 07:44:32 +0700

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  googleSheetsApiKey: 'AIzaSyBXTCDVZLRPsWanb_ab5Lp9GJa1upX8bf8',
-  characters: {
-    spreadsheetId: '1w_LXJm2Vsw11IlhyuwaMJEWQLT36kgrJ3fJUQwA37fg',
-    worksheetName: 'AngularNotify1',
-  },
-};
